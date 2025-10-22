1bf1a0ac95fc4da300302deae898904355739b3c
ytuik97 <cepc16@gmail.com>
2025-10-21 22:42:44 -0300

    cria modularizacao da funcao criar lista

--- scripts/scritp.js
+++ scripts/scritp.js
@@ -1,51 +1,13 @@
-const inputItem = document.getElementById("input-item");
+import {criarItemDaLista} from "./criarItemDaLista.js";
+
 const listaDeCompras = document.getElementById("lista-de-compras");
 const botaoAdicionar = document.getElementById("adicionar-item");
 
-let contador = 0;
-
 botaoAdicionar.addEventListener("click", (evento) => {
     evento.preventDefault();
-    if(inputItem.value === ""){
-        alert("Digite um item!")
-        return
-    }
-
-    const itemDaLista = document.createElement("li");
-    const containerItemDaLista = document.createElement("div");
-    containerItemDaLista.classList.add("lista-item-container");
-    const inputCheckbox = document.createElement("input");
-    inputCheckbox.type = "checkbox";
-    inputCheckbox.id = "checkbox-" + contador++;
-    const nomeItem = document.createElement("p");
-    nomeItem.innerText = inputItem.value;
-
-    inputCheckbox.addEventListener("click", function(){
-        if(inputCheckbox.checked){
-            nomeItem.style.textDecoration = "line-through";
-        } else {
-            nomeItem.style.textDecoration = "none";
-        }
-    })
-
-    containerItemDaLista.appendChild(inputCheckbox);
-    containerItemDaLista.appendChild(nomeItem);
-
-
-    const diaDaSemana = new Date().toLocaleDateString("pt-BR", {weekday: "long"});
-    const data = new Date().toLocaleDateString("pt-BR")
-    const hora = new Date().toLocaleTimeString("pt-BR", {hour: "numeric", minute: "numeric"})
-    const dataCompleta = `${diaDaSemana} (${data}) às ${hora}`;
-    const itemData = document.createElement("p");
-    itemData.innerText = dataCompleta;
-    
-    itemData.classList.add("texto-data");
-
-    itemDaLista.appendChild(containerItemDaLista);
-    itemDaLista.appendChild(itemData);
+    const itemDaLista = criarItemDaLista();
     listaDeCompras.appendChild(itemDaLista);
-
-    verificarListaVazia()
+    verificarListaVazia();
 })
 
 const mensagemItemListaVazia = document.querySelector(".mensagem-lista-vazia");
